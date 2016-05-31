5e034fae07cb35f68ac229f0f0da57621fbf498d
Halil Ozercan <halilozercan@gmail.com>
2016-05-31 11:02:18 +0300

    added requests for installation and version is fixed

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 
 setup(
     name='ParallelDownload',
-    version='0.04',
+    version='0.06',
     description='A library to download large files in chunks.',
     author='Halil Ozercan',
     author_email='halilozercan@gmail',
     url='https://github.com/halilozercan/paralleldownload',
-    download_url = 'https://github.com/halilozercan/paralleldownload/tarball/0.04',
+    download_url = 'https://github.com/halilozercan/paralleldownload/tarball/0.06',
     entry_points={
         'console_scripts': [
             'pdownload = ParallelDownload.bin:main',
